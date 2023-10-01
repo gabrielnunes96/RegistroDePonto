0fc3271fdfd517730e2f5a63fb56c03f6769582f
gabrielnunes96 <gabriel.nunes96@hotmail.com>
2023-09-30 21:27:21 -0300

    folder management

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const employeesController = require("./controllers/employees-controller/employeesController.js");
+const employeesController = require("../controllers/employees-controller/employeesController.js");
 router.get("/getEmployees", employeesController.getAll);
 router.get("/getEmployeesById/:id", employeesController.getById);
 router.post("/insertEmployee", employeesController.insertEmployee);
